7388ef0ce74e60986874246d20c5f151bc26d838
shinjaewon99 <tlswodnjs99@naver.com>
2025-09-17 22:45:47 +0900

    중재자 패턴 AS_IS 구현

---
+++ src/behavioral/mediator/Main.java
@@ -0,0 +1,14 @@
+package behavioral.mediator;
+
+/**
+ * 객체들이 서로 직접 참조함에 따라 의존성이 높아짐
+ */
+public class Main {
+    public static void main(String[] args) {
+        User alice = new User("Alice");
+        User bob = new User("Bob");
+
+        alice.sendMessage("안녕 Bob!", bob);
+        bob.sendMessage("안녕 Alice!", alice);
+    }
+}
---
+++ src/behavioral/mediator/User.java
@@ -0,0 +1,18 @@
+package behavioral.mediator;
+
+public class User {
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public void sendMessage(String msg, User to) {
+        System.out.println(this.name + " -> " + to.name + " : " + msg);
+        to.receiveMessage(msg, this);
+    }
+
+    public void receiveMessage(String msg, User from) {
+        System.out.println(this.name + "님이 " + from.name + "로부터 메시지 수신: " + msg);
+    }
+}
